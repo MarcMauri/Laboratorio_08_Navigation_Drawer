b2b1c32ef026a727edc56b35f28d25705ee69be3
marcc992 <marcmauri92@gmail.com>
2017-11-14 13:03:13 +0100

    Agregada hamburguesa para abrir menu

--- app/src/main/java/app/android/mmauri/laboratorio_08_navigation_drawer/Activities/MainActivity.java
+++ app/src/main/java/app/android/mmauri/laboratorio_08_navigation_drawer/Activities/MainActivity.java
@@ -1,15 +1,42 @@
 package app.android.mmauri.laboratorio_08_navigation_drawer.Activities;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 import app.android.mmauri.laboratorio_08_navigation_drawer.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setToolbar();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(Gravity.START);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
